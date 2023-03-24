4a910dc2ddaab5697f703d70baff7b07ee3b4735
OBellochDiaz <odiaz@codec.ie>
2023-03-24 16:02:17 +0000

    Refactoring List component.

--- projects/hse-angular/src/lib/components/govuk-list/govuk-list-item.component.ts
+++ projects/hse-angular/src/lib/components/govuk-list/govuk-list-item.component.ts
@@ -1,11 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'govuk-list-item',
-  template: ''
+  template: `<ng-template><ng-content /></ng-template>`
 })
 export class GovukListItemComponent {
   @Input() public link?: string;
   @Input() public text?: string;
   @Input() public newTab: boolean = false;
+  
+  @ViewChild(TemplateRef)
+  template!: TemplateRef<any>;
 }
--- projects/hse-angular/src/lib/components/govuk-list/govuk-list.component.ts
+++ projects/hse-angular/src/lib/components/govuk-list/govuk-list.component.ts
@@ -1,13 +1,18 @@
-import { Component, ContentChildren, Input, QueryList } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ContentChildren, Input, QueryList } from '@angular/core';
 import { GovukListItemComponent } from './govuk-list-item.component';
 @Component({
   selector: 'govuk-list',
   templateUrl: './govuk-list.component.html'
 })
-export class GovukListComponent {
+export class GovukListComponent implements AfterViewInit {
   @Input() public classes?: string;
 
   @ContentChildren(GovukListItemComponent)
   public items!: QueryList<GovukListItemComponent>;
 
+  constructor(private changeDetectorRef: ChangeDetectorRef){}
+  
+  public ngAfterViewInit(): void {
+    this.changeDetectorRef.detectChanges();
+  }
 }
