e82e7e68ea3e5681a4f6ffe0e0a49712dc2889c9
Diego Santin <Diego Santin>
2023-05-25 12:22:35 -0300

    hiding buttons

--- projects/hse-angular/src/lib/components/govuk-formpage/govuk-formpage.component.ts
+++ projects/hse-angular/src/lib/components/govuk-formpage/govuk-formpage.component.ts
@@ -6,8 +6,13 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
 })
 export class GovukFormPageComponent {
     @Input() saveAndContinueText: string = "Save and continue";
+    
     @Input() hideBackButton: boolean = false;
+    @Input() hideSaveAndContinue: boolean = false;
+    @Input() hideSaveAndComeBack: boolean = false;
+
     @Input() processing: boolean = false;
+
     @Output() saveAndContinue = new EventEmitter();
     @Output() saveAndComeBack = new EventEmitter();
 }
